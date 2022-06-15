94febd466428ecc43e72aa36afd1e68d298e7241
Xuan <97ssps30212@gmail.com>
2022-06-15 15:59:02 +0800

    Update README.md

--- src/types/TypistProps.ts
+++ src/types/TypistProps.ts
@@ -3,21 +3,14 @@ export type TypedLines = (string | React.ReactElement | null)[];
 export type Splitter = (str: string) => string[];
 
 export type TypistProps = {
-  /** Contents that will be animated as a typewriter. */
   children: React.ReactNode;
   typingDelay?: number;
   backspaceDelay?: number;
   loop?: boolean;
-  /** Pause the current typing animation. */
   pause?: boolean;
-  /** Called whenever the typing animation has been finished.*/
   onTypingDone?: () => void;
-  /** Used to split a string. */
   splitter?: Splitter;
-  /** Will be inserted after the last character. */
   cursor?: string | React.ReactElement;
-  /** Whenever this value changes, recreate a TypistCore instance to restart the typing animation. */
   restartKey?: any;
-  /** Stop the typing animation and show the final result if this property is `true` */
   disabled?: boolean;
 };
