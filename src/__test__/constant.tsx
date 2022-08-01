45256b8a14c52713fbdd2c65093dc062a0c9d1b4
Xuan <97ssps30212@gmail.com>
2022-08-01 11:01:48 +0800

    test: add test for getTypedChildrenArray

--- src/__test__/constant.tsx
+++ src/__test__/constant.tsx
@@ -1,11 +1,29 @@
 import React from 'react';
 import Typist from '..';
 
+export const textsArray = [
+  ['a'],
+  ['ab'],
+  ['ab', 'c'],
+  ['ab', 'cd'],
+  ['ab', 'c'],
+  ['ab'],
+  ['ab', 'e'],
+  ['ab', 'ef'],
+  ['ab', 'ef', 'gh'],
+];
+
 export const nestedChildren = (
-  <div className="first">
-    first
-    <div className="second">second</div>
-    <Typist.Backspace count={6} />
-    <div className="third">third</div>
+  <div className="ab">
+    ab
+    <div className="cd">cd</div>
+    <Typist.Backspace count={2} />
+    <div className="ef">ef</div>
+    <Typist.Paste>
+      <span>gh</span>
+    </Typist.Paste>
   </div>
 );
+
+export const WRAPPER_ID = 'wrapper';
+export const BUTTON_ID = 'button';
