d8b284224653b30e90397d2f1438a15075562af8
Xuan <97ssps30212@gmail.com>
2022-08-14 12:41:27 +0800

    feat: add Header component

--- dev/App.tsx
+++ dev/App.tsx
@@ -1,20 +1,22 @@
 import React, { useState } from 'react';
 import './App.css';
 import Typist from '../src';
+import Header from './components/Header';
 
 function App() {
   const [count, setCount] = useState(0);
   const [key, setKey] = useState(1);
 
   return (
-    <div className="App text-blue-400">
+    <div className='bg-slate-600 px-12 py-10 text-white '>
+      <Header />
       <button onClick={() => setKey(key + 1)}>key</button>
       <button onClick={() => setCount(count + 1)}>{count}</button>
       <br />
       <Typist
         typingDelay={100}
         disabled={count % 2 === 0}
-        cursor={<span className="cursor">|</span>}
+        cursor={<span className='cursor'>|</span>}
         restartKey={key}
       >
         This is a typo
