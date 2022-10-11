c5c3c06d95562c711a0a74d78f98d15a444bd51f
Xuan <97ssps30212@gmail.com>
2022-10-11 23:59:48 +0800

    test: complete testing for single mode

--- packages/react-use-calendar-component/src/app/components/StateInfo.tsx
+++ packages/react-use-calendar-component/src/app/components/StateInfo.tsx
@@ -6,16 +6,16 @@ interface Props {
 export default function StateInfo({ value, selectedDates }: Props) {
   return (
     <div className='text-white flex justify-between mt-5'>
-      <div className='flex flex-col'>
+      <div className='flex flex-col' data-testid='value'>
         <div className='text-lg text-rose-300'>value:</div>
         {value.map((v, index) => (
-          <span key={index}>{v.toLocaleDateString()}</span>
+          <span key={index}>{v.toDateString()}</span>
         ))}
       </div>
-      <div className='flex flex-col'>
+      <div className='flex flex-col' data-testid='selected-dates'>
         <div className='text-lg text-rose-300'>selected dates:</div>
         {selectedDates.map((date, index) => {
-          return <span key={index}>{date.toLocaleDateString()}</span>;
+          return <span key={index}>{date.toDateString()}</span>;
         })}
       </div>
     </div>
